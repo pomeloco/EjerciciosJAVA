445a57d6b5d2c176ee2ad55a10ee2f0c9642afe3
Francisco Castrelo <frannn_avellaneda@hotmail.com>
2017-09-19 16:04:09 -0300

    CommitDos.EjercicioOnce
    ejercicio hecho con parceo

--- EjercicioOnce.java
+++ EjercicioOnce.java
@@ -20,16 +20,22 @@ informar cuanto le corresponde de sueldo, restándole el 15% de aportes.
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        String Dato;
         float valorHoraTrabajo=0, cantidadHorasTrabajadas=0, sueldo=0;
         Scanner miSc = new Scanner(System.in);
         
         System.out.println("Ingrese el valor de la hora trabajada: ");
-        valorHoraTrabajo = miSc.nextFloat();
+        //valorHoraTrabajo = miSc.nextFloat();
+        Dato = miSc.next();
+        valorHoraTrabajo = Float.parseFloat(Dato);
         
         System.out.println("Ingrese cantidad de horas trabajadas: ");
-        cantidadHorasTrabajadas = miSc.nextFloat();
+        //cantidadHorasTrabajadas = miSc.nextFloat();
+        Dato = miSc.next();
+        cantidadHorasTrabajadas = Integer.parseInt(Dato);
         
-        sueldo = valorHoraTrabajo*cantidadHorasTrabajadas;
+        sueldo = (valorHoraTrabajo*cantidadHorasTrabajadas) - ((valorHoraTrabajo*cantidadHorasTrabajadas)*15/100);
+        //sueldo = sueldo - (sueldo*15/100);
         System.out.println("El sueldo correspondiente es de: "+sueldo);
         
     }
