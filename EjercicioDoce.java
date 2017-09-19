6cf13f154b000be5c380b3984db25066abd0704e
Francisco Castrelo <frannn_avellaneda@hotmail.com>
2017-09-18 21:14:45 -0300

    Commit1.EjercicioDoce
    Ejercicio Terminado

---
+++ EjercicioDoce.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejerciciodoce;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author pomelo
+ */
+public class EjercicioDoce {
+
+    /**
+     * @param args the command line arguments
+     * Se ingresa el importe de un producto importado y se le debe agregar el 35 % de impuestos
+internos.
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        float precioProductoImporado=0, precioTotal=0;
+        Scanner miSc = new Scanner(System.in);
+        
+        System.out.println("Ingrese importe de producto: ");
+        precioProductoImporado = miSc.nextFloat();
+        
+        precioTotal= precioProductoImporado+(precioProductoImporado*25/100);
+        System.out.println("El precio total es: "+precioTotal);
+    }
+    
+}
