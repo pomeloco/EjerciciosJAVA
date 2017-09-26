96994dca058b7156fe8c561268b70c4cda2c5007
Francisco Castrelo <frannn_avellaneda@hotmail.com>
2017-09-26 17:57:18 -0300

    CommitUno.Juego
    terminar el juego

---
+++ AdivinaElNumero.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package adivinaelnumero;
+
+import com.sun.java.swing.plaf.windows.WindowsInternalFrameTitlePane;
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ *
+ * @author alumno
+ */
+public class AdivinaElNumero {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Random iRandom = new Random();
+        Scanner scScanner = new Scanner(System.in);
+        String szDatoAux;
+        Integer iNumRandom,iNumeroUsuario;
+        
+        iNumRandom = (int)(iRandom.nextDouble()*10+1);  //Genera random de 1 a 10
+        //iNumRandom = iRandom.nextInt(10); //aparece el 0
+        
+        System.out.println("Ingrese numero: ");
+        szDatoAux = scScanner.next();
+        iNumeroUsuario = Integer.parseInt(szDatoAux);
+        
+        if(iNumeroUsuario == iNumRandom){
+            
+            System.out.println("Felicitaciones, GANASTE!");
+            
+        }else{
+            
+            System.out.println("Segui participando!");
+            
+            if(iNumeroUsuario < iNumRandom){
+                System.out.println("Te falto!!");
+            }else{
+                System.out.println("Te pasaste!!");
+            }
+            
+        }
+    }
+    
+}
+
+/**
+ * Informar si: 
+ * A) si le erro por un punto " ksi ksi"
+ * B) si le erro por dos puntos "cerca"
+ * C) si le erro por tres puntos "ni cerca"
+ * D) si le erro por cuatro puntos "bien lejos"
+ * E) para todos los demas "sos horrible en este juego"
+ */
